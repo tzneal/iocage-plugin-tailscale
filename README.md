b73ecb1087577eb76c6ad05e693c7f06f5dfba37
Todd Neal <todd@tneal.org>
2021-12-06 21:21:15 -0600

    interim

---
+++ README.md
@@ -0,0 +1,7 @@
+# Installation
+
+```bash
+curl https://raw.githubusercontent.com/tzneal/iocage-plugin-tailscale/12.2-RELEASE/tailscale.json -o tailscale.json
+iocage fetch --update -P tailscale.json
+```
+
--- post_install.sh
+++ post_install.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-# remove all of this once quarterly is tailscale 1.19+
+# <-- remove all of this once the standard tailscale package version is >= 1.19 -->
+GIT_HASH=eeccbccd08626fd968f849a86eac9d39ebd94f12
 pkg install -y git-lite go
 git clone https://github.com/tailscale/tailscale.git
+git checkout "$(GIT_HASH)"
 cd tailscale/cmd/tailscale
 go build
 cp tailscale /usr/local/bin
 cd ../tailscaled
 go build
 cp tailscaled /usr/local/bin
+# <-- remove the above -->
 
 sysrc -f /etc/rc.conf tailscaled_enable="YES"
 service tailscaled start
